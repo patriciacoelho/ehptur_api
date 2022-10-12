205c1d5168f44fcc51f4f890912b190688b0e1f9
Patrícia Coelho <patriciacoelho.slv@gmail.com>
2022-10-12 14:59:47 -0300

    fix: adiciona métodos parse ao model de Itinerary

--- api/models.py
+++ api/models.py
@@ -57,6 +57,15 @@ class Itinerary(BaseModel):
     trip_id: str
     operator_id: str
 
+    def to_json(self):
+        return jsonable_encoder(self, exclude_none=True)
+
+    def to_bson(self):
+        data = self.dict(by_alias=True, exclude_none=True)
+        if data.get("_id") is None:
+            data.pop("_id", None)
+        return data
+
 class Tagged(BaseModel):
     already_know: bool
     user_id: str
