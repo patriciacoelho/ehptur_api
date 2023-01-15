cb85f721fa4d0d17ef12ac66a6fc591dd3812497
Patrícia Coelho <patriciacoelho.slv@gmail.com>
2023-01-15 11:26:39 -0300

    fix: adiciona app.run no index

--- index.py
+++ index.py
@@ -4,4 +4,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from api import create_app
 
-app = create_app('production')
+app = create_app()
+
+if __name__ == "__main__":
+    app.run()
