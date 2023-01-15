aa09a5066f9919bfd127534c602404ac4bb573fa
Patrícia Coelho <patriciacoelho.slv@gmail.com>
2023-01-15 10:53:41 -0300

    fix: atualiza o index.py

--- index.py
+++ index.py
@@ -1,1 +1,7 @@
-from wsgi import app
+import os, sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from api import create_app
+
+app = create_app('production')
